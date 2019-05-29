81689f60a7747fc6a3097ed558760e2f7de3c8a9
blackk100 <blackk100@users.noreply.github.com>
2019-05-29 19:08:13 +0530

    Initial re-write
    Shifted to CDN. Removed all files which aren't required.
    Added "vanilla" navbar to all pages. Replaced the scroll to top button on all pages (stlll need to style it to come into view at the bottom left of the page after scrolling down)
    Currently rewriting the home page.

--- docs/assets/js/contact.js
+++ docs/assets/js/contact.js
@@ -1,16 +1,23 @@
-(function () {
-  emailjs.init('user_gWyGwhhTYkdkYmjhj52AZ')
-})()
-window.onload = function () {
-  document.getElementById('contact-form').addEventListener('submit', function (event) {
-    event.preventDefault()
-    emailjs.sendForm('default_service', 'contact_form', this)
-      .then(function (response) {
-        console.log('SUCCESS. status=%d, text=%s', response.status, response.text)
-        alert('Your message has been successfully delivered!')
-      }, function (err) {
-        console.log('FAILED. error=', err)
-        alert('An error occurred!\r\nKindly manually email us your message (and preferably this error as well) instead.\r\n\r\nERROR: ' + err.name + '\r\nERROR MESSAGE: ' + err.message)
-      })
+// Script that is run for the contact form
+$(document).ready(function () {
+  vex.defaultOptions.className = 'vex-theme-default' // Initialises vex.js (for fancy alerts)
+
+  emailjs.init('user_gWyGwhhTYkdkYmjhj52AZ') // Initialises the email.js client
+
+  $('#contact-form').submit(function (event) { // What to do when the form is submitted
+    event.preventDefault() // Prevent default click behaviour
+
+    // Send the form data to email.js servers
+    emailjs.sendForm('default_service', 'contact_form', {
+      'user_name': $('#user_name').val(),
+      'user_email': $('#user_email').val(),
+      'message': $('#message').val()
+    }).then(function (response) { // Alert to show when submit was successful
+      console.log('SUCCESS. status=%d, text=%s', response.status, response.text)
+      vex.dialog.alert('Your message has been successfully delivered!')
+    }, function (err) { // Alert to show when the submit failed successfully
+      console.log('FAILED. error=', err)
+      vex.dialog.alert('An error occurred!\r\nKindly manually email us your message (and preferably this error as well) instead.\r\n\r\nERROR: ' + err.name + '\r\nERROR MESSAGE: ' + err.message)
+    })
   })
-}
+})
