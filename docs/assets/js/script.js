48eea3ed752a356174b6c009fec1600fc4f51ee9
blackk100 <blackk100@users.noreply.github.com>
2019-06-01 23:09:17 +0530

    Hompe page done. All assets are locally hosted.
    Slide-out effect on the footer needs to be done.
    Rewritten navbar and footer exist on all pages.

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,17 +1,39 @@
-// Debugging function used to print highlightable DOM elements to the Chrome console.
-// Usage: $(...).log()
+/* Debugging function used to print highlightable DOM elements to the Chrome console.
+ * May work on other browsers too.
+ * Usage: $(...).log()
+ */
 $.fn.log = function () {
   console.log.apply(console, this)
   return this
 }
 
+/* onClick event handler for links pointing to the same page.
+ * Called by #go-to-top (see below) and and by navbar/footer links (via 'onclick="scrollToTop(event);"' attribute).
+ */
+var scrollToTop = function (evt) {
+  evt.preventDefault() // Prevent default anchor click behavior.
+
+  $('html, body').animate({
+    scrollTop: 0
+  }, 'slow')
+
+  return false
+}
+
 // Functions to run after the page has loaded.
 $(document).ready(function () {
-  window.scrollTo(0, 0) // Scrolls to top of the window upon page load
+  // Scrolls to top of the window upon page load.
+  $('html, body').scrollTop(0)
+  $('html, body').scrollLeft(0)
 
-  // All social media links are reduced to 50% opacity on page load
-  $('.social-link').each(function () {
-    $(this).children('a').fadeTo(1, 0.5)
+  // Adds the top-level class to all 1st children of <body>.
+  $('body').children().each(function () {
+    $(this).addClass('top-level')
+  })
+
+  // Sets up Jarallax (parallax scrolling effect on images).
+  $('.jarallax').jarallax({
+    speed: 0.4
   })
 
   // Dropdowns open when hovered upon.
@@ -23,34 +45,64 @@ $(document).ready(function () {
     $(this).find('.dropdown-menu').removeClass('show')
   })
 
-  // Animated scrolling within the page
-  $('a').click(function (event) {
-    var hash = this.hash // The '#xyz' part of the href
+  /* All buttons/elements containing icons that are supposed to have a "fade" animation.
+   * They have 50% opacity initially.
+   * Opacity is increased to 100% when hovered upon.
+   * Opacity is reduced to 50% when the pointer is removed.
+   */
+  $('.fadey-button').each(function () {
+    $(this).find('i').each(function () {
+      $(this).fadeTo(1, 0.5)
 
-    if (hash !== '') { // If hash has a value, scroll to that location
-      event.preventDefault() // Prevent default anchor click behavior
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1000, function () {
-        window.location.hash = hash // Add hash (#) to URL when done scrolling (for emulating default click behavior)
+      $(this).hover(function () {
+        $(this).fadeTo('fast', 1)
+      }, function () {
+        $(this).fadeTo('fast', 0.5)
       })
-    } else if (this.attr('id') === 'scrollToTop') { // Else, scroll to Top if its the scrollToTop button
-      event.preventDefault() // Prevent default anchor click behavior
-      $('html, body').animate({
-        scrollTop: 0
-      }, 1000, function () {
-        window.location.hash = hash // Add hash (#) to URL when done scrolling (for emulating default click behavior)
-      })
+    })
+  })
+
+	// Colour fading for the buttons. More may be added later.
+  $('.btn-primary').each(function () {
+    $(this).hover(function () {
+      $(this).animate({
+        backgroundColor: '#149DCC'
+      }, 'fast')
+    }, function () {
+      $(this).animate({
+        backgroundColor: 'rgba(0, 0, 0, 0)'
+      }, 'fast')
+    })
+  })
+
+  // Animated scrolling to the next "main" element.
+  $('.scroll-below').click(function (event) {
+    event.preventDefault() // Prevent default anchor click behavior.
+
+    $('html, body').animate({
+      // Subtracting the navbar's height cause otherwise the contents are covered by the navbar.
+      scrollTop: $(this).parents('.top-level').next().offset().top - $('nav').height()
+    }, 'slow')
+
+    $()
+
+    return false
+  })
+
+  // Hide the Scroll to Top button iitially.
+  $('#scroll-to-top').hide()
+
+  // Show the Scroll to Top button if the window isn't already at the top.
+  $(window).scroll(function () {
+    if ($(window).scrollTop() !== 0) {
+      $('#scroll-to-top').show('fast')
+    } else {
+      $('#scroll-to-top').hide('fast')
     }
   })
 
-  // All social media links are animated to have 100% opacity when hovered upon.
-  // Opacity is reduced to 50% when the pointer is removed.
-  $('.social-link').hover(
-    function () {
-      $(this).children('a').fadeTo('fast', 1)
-    }, function () {
-      $(this).children('a').fadeTo('fast', 0.5)
-    }
-  )
+  // Animated scrolling to the top of the page when the Scroll to Top button is clicked.
+  $('#scroll-to-top').click(function (event) {
+    scrollToTop(event)
+  })
 })
